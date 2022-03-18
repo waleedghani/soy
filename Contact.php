137209923ff0fad40daf44fb12d5fc6824c01667
Zain Ahmed <zain.ahmed@inoviotech.local>
2022-03-18 22:26:20 +0500

    zip

---
+++ Contact.php
@@ -0,0 +1,112 @@
+<?php include 'inc/app.php'; ?>
+<!-- css file starts here -->
+<link rel="stylesheet" href="css/contact.css">
+<!-- css file ends here -->
+<!-- page header starts here -->
+<section class="page-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-banner-wrapper text-center">
+                    <h1>Contact Us</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- page header ends here -->
+<!-- contact form css starts here -->
+<section class="contact-form">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="contact-form-heading-wrapper text-center">
+                    <h3>We Are Always Here To Help You</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas quis ipsum suspendisse ultrices gravida dictum fusce.</p>
+                </div>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+
+                <div class="contact-form-wrapper">
+                    <form action="">
+                        <div class="form-group row">
+                            <div class="col-lg-6">
+                                <input type="text" placeholder="Name" class="form-control">
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="text" placeholder="Name" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <textarea name="" class="form-control" placeholder="Message" id="" cols="30" rows="6"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button class="form-btn form-control">Continue</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
+    </div>
+</section>
+<!-- contact form css ends here -->
+<!-- contact form info starts here -->
+<section class="contact-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 align-self-center">
+                <div class="contact-info-wrapper text-center">
+                    <div class="contact-info-img">
+                        <figure>
+                            <img src="img/mail.svg" class="img-fluid" alt="">
+                        </figure>
+                    </div>
+                    <div class="contact-content-wrapper">
+                        <h4>Email us</h4>
+                        <p>Email us for general queries, including marketing and partnership opportunities.</p>
+                        <a href="mailto:info@Soltanning.co.uk">info@Soltanning.co.uk</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 align-self-center">
+                <div class="contact-info-wrapper text-center">
+                    <div class="contact-info-img">
+                        <figure>
+                            <img src="img/phone.svg" class="img-fluid" alt="">
+                        </figure>
+                    </div>
+                    <div class="contact-content-wrapper">
+                        <h4>Call us</h4>
+                        <p>Call us to speak to a member of our team. We are always happy to help.</p>
+                        <a href="tel:012-3456-7890">012-3456-7890</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 align-self-center">
+                <div class="contact-info-wrapper text-center">
+                    <div class="contact-info-img">
+                        <figure>
+                            <img src="img/mail.svg" class="img-fluid" alt="">
+                        </figure>
+                    </div>
+                    <div class="contact-content-wrapper">
+                        <h4>Our Head Office</h4>
+                        <p>8182 Fulton Rd.
+                            Amirican Plain, MA 02130</p>
+                        <a href="#">Support Center</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- contact form info ends here -->
+<!-- footer starts here -->
+<?php include 'inc/footer.php'; ?>
+<!-- footer ends here -->
