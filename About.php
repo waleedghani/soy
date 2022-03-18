cf886b53cd418e35e334b56559116b6800275b8e
Muhammad Shariq <muhammad.shariq@inoviotech.local>
2022-03-18 21:20:28 +0500

    new

---
+++ About.php
@@ -0,0 +1,129 @@
+<!-- App Include Here -->
+<?php include 'inc/app.php';?>
+<!-- App Include Here -->
+
+<link rel="stylesheet" href="css/about.css">
+
+<!-- Inner banner section  -->
+<section class="inner-banner-sec">
+   <div class="inner-banner-img">
+       <img class="img-fluid" src="img/inner-banner.png" alt="">
+   </div>
+
+   <div class="inner-banner-head">
+       <h2>About Us</h2>
+   </div>
+    
+</section>
+<!-- End of Inner Banner section  -->
+
+
+<!-- Reveiws section  -->
+<section class="review-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="platform">
+                    <h2>We Are The Best Platform To <br>
+                       Reviews Of Companies </h2>
+
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                       <div class="platform-btn">
+                           <a href="">Report Now</a>
+                       </div>
+                </div>
+
+
+            </div>
+
+            <div class="col-md-6">
+                <div class="platform-img">
+                    <img class="img-fluid" src="img/about1.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End of Reviews section  -->
+
+
+
+
+
+<!-- Number section  -->
+<section class="number-sec">
+    <div class="container">
+        <div class="Number-head">
+            <h2>Our Number Says It All</h2>
+            <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="number-sec-all">
+                    <div class="number1 color">
+                        <h3>90 K</h3>
+                        <p>Reviews on SOY big</p>
+                    </div>
+
+                    <div class="number2 color">
+                        <h3>1.3 M</h3>
+                        <p>Websites reviewed</p>
+                    </div>
+
+                    <div class="number3 color">
+                        <h3>2018</h3>
+                        <p>SOY BUG launched</p>
+                    </div>
+
+                    <div class="number4 color">
+                        <h3>302</h3>
+                        <p>Employees</p>
+                    </div>
+
+                    <div class="number5 color">
+                        <h3>32</h3>
+                        <p>Nationalities</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End of Number section  -->
+
+<!-- Reveiws section  -->
+<section class="review-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+             
+            <div class="platform-img">
+                    <img class="img-fluid" src="img/about2.png" alt="">
+                </div>
+
+            </div>
+
+            <div class="col-md-6">
+            <div class="platform">
+                    <h2>We Are The Best Platform To <br>
+                       Reviews Of Companies </h2>
+
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                       <div class="platform-btn">
+                           <a href="">Report Now</a>
+                       </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End of Reviews section  -->
+
+<!-- Footer Include Here -->
+<?php include 'inc/footer.php';?>
+<!-- Footer Include Here -->
